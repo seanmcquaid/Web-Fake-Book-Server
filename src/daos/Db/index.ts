4b9e96bc1dcdfab648098a247d1274acbccd11fa
seanmcquaid <sean.mcquaid2@verizon.net>
2021-03-02 21:17:28 -0500

    removed console log but it works

--- src/daos/Db/index.ts
+++ src/daos/Db/index.ts
@@ -4,8 +4,6 @@ dotenv.config();
 
 const MONGO_URL = process.env.MONGO_URL || '';
 
-console.log(MONGO_URL);
-
 export const connectDb = async () => {
   await mongoose.connect(MONGO_URL, {
     useNewUrlParser: true,
